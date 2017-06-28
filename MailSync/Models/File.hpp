1063742c1364453e87b3d78f6388dd87734aacd7
Ben Gotow <ben@foundry376.com>
2017-06-28 16:25:30 -0700

    Write files to same folder structure expected by N1

--- MailSync/Models/File.hpp
+++ MailSync/Models/File.hpp
@@ -34,7 +34,9 @@ public:
     File(SQLite::Statement & query);
   
     string filename();
-                          
+    string safeFilename();
+    string partId();
+        
     string tableName();
     string constructorName();
 
