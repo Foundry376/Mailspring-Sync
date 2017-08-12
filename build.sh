d233c28f1a01b8cd628821822648f99dca28584e
Ben Gotow <ben@foundry376.com>
2017-08-11 19:23:31 -0700

    Fix all the relative path crap in build.sh

--- build.sh
+++ build.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
 
-cd "${0%/*}"
-
 if [[ "$OSTYPE" == "darwin"* ]]; then
   xcodebuild -scheme MailSync -configuration Release;
 
 elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+  export MAILSYNC_DIR=$( cd $(dirname $0) ; pwd -P );
+  cd "$MAILSYNC_DIR";
+
   # install libetpan from source
-  mkdir ~/libetpan
-  cd ~/libetpan
+  mkdir -p temp
+  cd ./temp
   git clone --depth=1 https://github.com/dinhviethoa/libetpan
-  cd libetpan
+  cd ./libetpan
   ./autogen.sh
   make >/dev/null
   sudo make install prefix=/usr >/dev/null
 
   # build mailcore2
-  cd "${0%/*}/Vendor/mailcore2"
+  cd "$MAILSYNC_DIR/Vendor/mailcore2"
   mkdir -p build
   cd build
   cmake ..
   make
 
   # build mailsync
-  cd "${0%/*}"
+  cd "$MAILSYNC_DIR"
   cmake .
   make
 
   # copy build product into the client working directory
-  cp mailsync ../client/app
+  cp "$MAILSYNC_DIR/mailsync" "$MAILSYNC_DIR/../app"
 
 else
   echo "Mailsync does not build on $OSTYPE yet.";
