fafac60f517575408a9ea52ed8f26486fba9d55f
Ben Gotow <ben@foundry376.com>
2017-08-12 17:45:29 -0700

    Make linux build quieter

--- build.sh
+++ build.sh
@@ -5,29 +5,39 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 
 elif [[ "$OSTYPE" == "linux-gnu" ]]; then
   export MAILSYNC_DIR=$( cd $(dirname $0) ; pwd -P );
-  cd "$MAILSYNC_DIR";
+  export DEP_BUILDS_DIR=/tmp/mailsync-build-deps
 
-  mkdir -p temp
+  # we cache this directory between builds to make CI faster.
+  # if it exists, just run make install again, otherwise pull
+  # the libraries down and build from source.
+  if [ -d "$DEP_BUILDS_DIR" ]; then
+    cd "$DEP_BUILDS_DIR/curl-7.50.2"
+    sudo make install prefix=/usr >/dev/null
+    cd "$DEP_BUILDS_DIR/libetpan"
+    sudo make install prefix=/usr >/dev/null
 
-  # Install curl from source because the Ubuntu trusty version is too old
-  cd "$MAILSYNC_DIR/temp"
-  sudo apt-get build-dep curl
-  wget http://curl.haxx.se/download/curl-7.50.2.tar.bz2
-  tar -xvjf curl-7.50.2.tar.bz2
-  cd curl-7.50.2
+  else
+    mkdir "$DEP_BUILDS_DIR"
 
-  ./configure
-  make
-  sudo make install prefix=/usr >/dev/null
-  sudo ldconfig
+    # Install curl from source because the Ubuntu trusty version is too old
+    cd "$DEP_BUILDS_DIR"
+    sudo apt-get build-dep curl
+    wget http://curl.haxx.se/download/curl-7.50.2.tar.bz2
+    tar -xjf curl-7.50.2.tar.bz2
+    cd curl-7.50.2
+    ./configure
+    make >/dev/null
+    sudo make install prefix=/usr >/dev/null
+    sudo ldconfig
 
-  # install libetpan from source
-  cd "$MAILSYNC_DIR/temp"
-  git clone --depth=1 https://github.com/dinhviethoa/libetpan
-  cd ./libetpan
-  ./autogen.sh
-  make >/dev/null
-  sudo make install prefix=/usr >/dev/null
+    # install libetpan from source
+    cd "$DEP_BUILDS_DIR"
+    git clone --depth=1 https://github.com/dinhviethoa/libetpan
+    cd ./libetpan
+    ./autogen.sh
+    make >/dev/null
+    sudo make install prefix=/usr >/dev/null
+  fi
 
   # build mailcore2
   cd "$MAILSYNC_DIR/Vendor/mailcore2"
