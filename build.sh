131f0038090486c0abad4205c2c62e638d88677c
Ben Gotow <ben@foundry376.com>
2017-10-03 00:17:43 -0500

    On Linux, link against most deps statically, bundle remaining with product

--- build.sh
+++ build.sh
@@ -11,9 +11,14 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
   cd "$MAILSYNC_DIR"
   gem install xcpretty;
   set -o pipefail && xcodebuild -scheme mailsync -configuration Release | xcpretty;
-  cp "$APP_ROOT_DIR/mailsync" "$APP_DIST_DIR/"
-  if [ -e "$APP_ROOT_DIR/mailsync.dSYM.zip" ]; then
-    cp "$APP_ROOT_DIR/mailsync.dSYM.zip" "$APP_DIST_DIR/";
+
+  # the xcodebuild copies the build products to the APP_ROOT_DIR and codesigns
+  # them for us. We just need to tar them up and move them to the artifacts folder
+  cd "$APP_ROOT_DIR"
+  if [ -e "mailsync.dSYM.zip" ]; then
+    tar -cf "$APP_DIST_DIR/mailsync.tar" mailsync.dSYM.zip mailsync
+  else
+    tar -cf "$APP_DIST_DIR/mailsync.tar" mailsync
   fi
 
 elif [[ "$OSTYPE" == "linux-gnu" ]]; then
@@ -39,9 +44,8 @@ elif [[ "$OSTYPE" == "linux-gnu" ]]; then
     wget -q http://curl.haxx.se/download/curl-7.50.2.tar.bz2
     tar -xjf curl-7.50.2.tar.bz2
     cd curl-7.50.2
-    ./configure --quiet
+    ./configure --disable-ftp --disable-ftps --disable-gopher --disable-dict --disable-imap --disable-imaps --disable-pop3 --disable-pop3s --disable-rtsp --disable-smb --disable-smtp --disable-smtps --disable-telnet --disable-tftp --quiet --disable-shared --enable-static --without-libidn >/dev/null
     make >/dev/null
-    sudo make install prefix=/usr >/dev/null
     sudo ldconfig
 
     # install libetpan from source
@@ -68,9 +72,19 @@ elif [[ "$OSTYPE" == "linux-gnu" ]]; then
   cmake .
   make
 
-  # copy build product into the client working directory
-  cp "$MAILSYNC_DIR/mailsync" "$APP_ROOT_DIR/"
-  cp "$MAILSYNC_DIR/mailsync" "$APP_DIST_DIR/"
+  # copy build product into the client working directory. There are some libraries
+  # we can't statically link against, so we copy those into the directory as well
+  # and wrap the executable in a script that prefixes the library search path so
+  # local lib versions are found.
+  cp "$MAILSYNC_DIR/mailsync" "$APP_ROOT_DIR/mailsync.bin"
+  ldd "$MAILSYNC_DIR/mailsync" | grep "=> /" | awk '{print $3}' | xargs -I '{}' cp -v '{}' "$APP_ROOT_DIR"
+
+  printf "#!/bin/bash\nLD_LIBRARY_PATH=\". ${LD_LIBRARY_PATH}\" ./mailsync.bin" > "$APP_ROOT_DIR/mailsync"
+  chmod +x "$APP_ROOT_DIR/mailsync"
+
+  # Zip this stuff up so we can push it to S3 as a single artifacts
+  cd "$APP_ROOT_DIR"
+  tar -cf "$APP_DIST_DIR/mailsync.tar" *.so* mailsync mailsync.bin --wildcards
 
 else
   echo "Mailsync does not build on $OSTYPE yet.";
